02a5964f8a119c33ce76519301d3de1796de8e17
y-ishii <root@localhost.localdomain>
2019-07-17 16:01:45 +0900

    4kan

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,11 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
-  end
-  
-  test "should get root" do
-    get root_url
+ test "should get home" do
+    get static_pages_home_url
+    
     assert_response :success
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
